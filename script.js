2d9f09e87e237493f33a3ee7769dbbee80c554b3
Marlon Almino <alminomarlon@gmail.com>
2022-02-15 19:12:31 -0300

    function 'lightColor' implemented

--- script.js
+++ script.js
@@ -18,4 +18,21 @@ let shuffleOrder = () => {
   let colorOrder = Math.floor(Math.random() * 4)
   order[order.length] = colorOrder
   clickedOrder = []
+
+  for (let i in order) {
+    let elementColor = createColorElement(order[i])
+    lightColor(elementColor, Number(i) + 1)
+  }
+}
+
+let lightColor = (element, number) => {
+  number = number * 500
+
+  setTimeout(() => {
+    element.classList.add('selected')
+  }, number - 250)
+
+  setTimeout(() => {
+    element.classList.remove('selected')
+  })
 }
